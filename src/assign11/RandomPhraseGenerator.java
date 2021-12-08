0630695e57c01b36a7164b0eb9948ca5fabcb145
Andrew Sumsion <andrew.sumsion7@gmail.com>
2021-12-07 17:32:03 -0700

    documentation

--- src/assign11/RandomPhraseGenerator.java
+++ src/assign11/RandomPhraseGenerator.java
@@ -3,7 +3,17 @@ package assign11;
 import java.io.IOException;
 import java.util.Random;
 
+/**
+ * Main class of our program which contains our main method
+ *
+ * @author Andrew Sumsion & Dillon Otto
+ */
 public class RandomPhraseGenerator {
+    /**
+     * Loads the grammar file at args[0] and uses it to generate args[1] many random phrases
+     * @param args command line arguments
+     * @throws IOException if an error occurred while reading the grammar file
+     */
     public static void main(String[] args) throws IOException {
         if(args.length < 2) {
             System.out.println("Invalid input!");
